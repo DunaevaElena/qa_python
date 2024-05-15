aef08d07d53dc719cf7fcbf06a3a1e762beb76a1
Elena Dunaeva <elena.dunaeva@sbermarket.ru>
2024-05-15 12:11:16 +0300

    fix

--- tests.py
+++ tests.py
@@ -5,22 +5,6 @@ from main import BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
-
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
     @pytest.mark.parametrize('name', ['Book1', 'Book2'])
@@ -49,7 +33,7 @@ class TestBooksCollector:
 
         collector5.add_new_book('Book5')
         collector5.set_book_genre('Book5', 'Фантастика')
-        assert collector5.get_book_genre('Book5') == 'Фантастика'
+        assert collector5.get_books_with_specific_genre('Book5') == 'Фантастика'
 
     def test_get_books_genre(self):
         collector6 = BooksCollector()
